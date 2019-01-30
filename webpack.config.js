dd550cd527249fbb9c10c8a384d73f0526791a79
Matthew Heck <mheck@unitedincome.com>
2019-01-30 12:53:56 -0500

    build process

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: glob.sync('./components/**/index.js').reduce((entries, entry) => {
+    return Object.assign(entries, {[entry.split('/')[2]]: entry});
+  }, {}),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: '@unitedincome/components',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {test: /\.jsx?$/, use: ['babel-loader'], exclude: /node_modules/},
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              data: '@import "./constants/sass/manifest.scss";',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      path: path.resolve(__dirname, 'dist'),
+    }),
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
+    'react-router': {
+      commonjs: 'react-router',
+      commonjs2: 'react-router',
+      amd: 'ReactRouter',
+      root: 'ReactRouter',
+    },
+    classnames: {
+      commonjs: 'classnames',
+      commonjs2: 'classnames',
+      amd: 'ClassNames',
+      root: 'ClassNames',
+    },
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
+};
