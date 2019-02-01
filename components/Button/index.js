424181b72420711b0084939a8dc019838f958863
Matthew Heck <mheck@unitedincome.com>
2019-02-01 13:12:45 -0500

    add explanation of new prop check

--- components/Button/index.js
+++ components/Button/index.js
@@ -53,22 +53,31 @@ const Button = ({
   );
 };
 
-const exclusiveProps = (exclusives) => (props, propName, componentName) => {
-  const multiples = [];
+/**
+ * Exclusive Props Check
+ *
+ * For a given prop, this checks that that prop does not coexist with other props. In other words, if the prop of "onClick" is included, we should not allow "to" to be defined in addition.
+ * @param {array} exclusives - Array of strings defining what also cannot be defined.
+ * @returns {function} the prop check
+ */
+function exclusiveProps(exclusives) {
+  return (props, propName, componentName) => {
+    const multiples = [];
 
-  for (const exclusive of exclusives) {
-    if (typeof props[exclusive] !== 'undefined') {
-      multiples.push(exclusive);
+    for (const exclusive of exclusives) {
+      if (typeof props[exclusive] !== 'undefined') {
+        multiples.push(exclusive);
+      }
     }
-  }
-  if (multiples.length > 0 && typeof props[propName] !== 'undefined') {
-    return new Error(
-      `Invalid prop ${propName} supplied to ${componentName}. Other exclusive props already defined: ${multiples.join(
-        ', '
-      )}`
-    );
-  }
-};
+    if (multiples.length > 0 && typeof props[propName] !== 'undefined') {
+      return new Error(
+        `Invalid prop ${propName} supplied to ${componentName}. Other exclusive props already defined: ${multiples.join(
+          ', '
+        )}`
+      );
+    }
+  };
+}
 
 Button.propTypes = {
   className: PropTypes.string,
