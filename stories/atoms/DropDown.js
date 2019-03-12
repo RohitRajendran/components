f863bca2a66772be0222d84f29e0757b614c7699
JamesIves <iam@jamesiv.es>
2019-03-12 17:53:43 -0400

    DropDown Component Progress

---
+++ stories/atoms/DropDown.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withKnobs, text, boolean, number} from '@storybook/addon-knobs';
+import {withReadme} from 'storybook-readme';
+import DropDown from '../../components/DropDown/DropDown';
+import DropDownReadme from '../../components/DropDown/DropDown.md';
+import {State, Store} from '@sambego/storybook-state';
+
+const store = new Store({
+  selectedExample1: '',
+});
+
+const stories = storiesOf('Atoms/DropDown', module);
+
+stories.addDecorator(withKnobs).add(
+  'default',
+  withReadme(DropDownReadme, () => (
+    <State store={store}>
+      {(state) => [
+        <DropDown
+          name="selectedExample1"
+          options={[
+            {
+              value: 'YES',
+              label: 'purchased',
+            },
+            {
+              value: 'NO',
+              label: 'a gift or inheritance',
+            },
+          ]}
+          selectTypeCSS="large-inline-on-light-select sentence-input sentence-large"
+          value={state.selectedExample1}
+          setField={(value) => store.set({selectedExample1: value})}
+          placeholder="1660 L Street"
+          key="selectedExample1"
+          label="Address"
+          required
+        />,
+      ]}
+    </State>
+  ))
+);
