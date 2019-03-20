24a0c547893d0d24e2b1c5cd7e35c2315aa60cb9
JamesIves <iam@jamesiv.es>
2019-03-20 12:35:42 -0400

    Update util.js

--- components/util.js
+++ components/util.js
@@ -57,6 +57,7 @@ export const ZipMask = {
  * @returns {regex[]} The mask
  */
 export const TickerMask = {
+  /* istanbul ignore next */
   mask(input) {
     return _.fill(Array(input.length), /[a-zA-Z]/);
   },
@@ -69,6 +70,7 @@ export const TickerMask = {
  * @returns {regex[]} the mask
  */
 export const CommaSeparatedMask = {
+  /* istanbul ignore next */
   mask(input) {
     return _.fill(Array(input.length + 1), /[A-Za-z, ]/);
   },
