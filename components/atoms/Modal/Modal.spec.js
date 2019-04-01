af3f3d3cc0566bdfdaf31ac2cbc4c4191239d43c
JamesIves <iam@jamesiv.es>
2019-04-01 13:37:59 -0400

    Document click tests

--- components/atoms/Modal/Modal.spec.js
+++ components/atoms/Modal/Modal.spec.js
@@ -1,63 +1,8 @@
 import test from 'tape';
 import React from 'react';
 import {mount} from 'enzyme';
+import {stub} from 'sinon';
 import Modal from './Modal';
-
-test('Modal - Should open the modal when the label is pressed', (t) => {
-  const props = {
-    defaultOpen: false,
-    preventClose: false,
-    label: 'Open Modal',
-  };
-
-  const component = mount(
-    <Modal {...props}>
-      <h1>Montezuma is the best cat</h1>
-      <p>You can place a paragraph about how good of a cat he is here</p>
-      <div className="uic--modal-bottom">
-        <p>Additional content to place at the bottom of the modal.</p>
-      </div>
-    </Modal>
-  );
-
-  component.find('.uic--modal-label').simulate('click');
-
-  t.deepEquals(
-    component.state().show,
-    true,
-    'Expect the show state to be true.'
-  );
-
-  t.end();
-});
-
-test('Modal - Should open the modal when the label is pressed via the keyboard.', (t) => {
-  const props = {
-    defaultOpen: false,
-    preventClose: false,
-    label: 'Open Modal',
-  };
-
-  const component = mount(
-    <Modal {...props}>
-      <h1>Montezuma is the best cat</h1>
-      <p>You can place a paragraph about how good of a cat he is here</p>
-      <div className="uic--modal-bottom">
-        <p>Additional content to place at the bottom of the modal.</p>
-      </div>
-    </Modal>
-  );
-
-  component.find('.uic--modal-close').simulate('keypress', {keyCode: 13});
-
-  t.deepEquals(
-    component.state().show,
-    true,
-    'Expect the show state to be true.'
-  );
-
-  t.end();
-});
 
 test('Modal - handleKeyPress', (t) => {
   t.plan(2);
@@ -117,6 +62,26 @@ test('Modal - handleClick', (t) => {
   component.instance().handleClick();
 
   t.deepEquals(component.state().show, true, 'Should toggle the show state.');
+
+  t.end();
+});
+
+test('HogwartsCabinet - handleDocumentClick', (t) => {
+  const props = {
+    defaultOpen: true,
+    preventClose: false,
+    label: 'Open Modal',
+  };
+
+  const component = new Modal(props);
+
+  const handleClickStub = stub(component, 'handleDocumentClick');
+
+  component.modalNode.current = {contains: () => false};
+
+  component.handleDocumentClick({});
+
+  t.true(handleClickStub.called, 'handleClick called');
 
   t.end();
 });
