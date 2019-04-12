372c1ef78a2a1b1ab82d101db0697184581f00e4
JamesIves <iam@jamesiv.es>
2019-04-12 15:30:06 -0400

    Fixing errors

--- components/atoms/icons/ArrowIcon/ArrowIcon.story.js
+++ components/atoms/icons/ArrowIcon/ArrowIcon.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import ArrowIcon from './ArrowIcon';
-import {text} from '@storybook/addon-knobs';
+import {text, select} from '@storybook/addon-knobs';
 import {withReadme} from 'storybook-readme';
 import ArrowIconReadme from './ArrowIcon.md';
 
@@ -13,6 +13,7 @@ const defaultProps = () => ({
   fill: text('fill', '#000'),
   height: text('height', '20'),
   width: text('width', '20'),
+  rotation: select('Background', ['down', 'up', 'right', 'left'], 'down'),
 });
 
 stories.add('default', () => <ArrowIcon {...defaultProps()} />);
--- components/atoms/icons/ExpandyCircleIcon/ExpandyCircleIcon.story.js
+++ components/atoms/icons/ExpandyCircleIcon/ExpandyCircleIcon.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import ExpandyCircleIcon from './ExpandyCircleIcon';
-import {text} from '@storybook/addon-knobs';
+import {text, select} from '@storybook/addon-knobs';
 import {withReadme} from 'storybook-readme';
 import ExpandyCircleIconReadme from './ExpandyCircleIcon.md';
 
@@ -13,6 +13,7 @@ const defaultProps = () => ({
   fill: text('fill', '#000'),
   height: text('height', '20'),
   width: text('width', '20'),
+  rotation: select('Background', ['down', 'up', 'right', 'left'], 'down'),
 });
 
 stories.add('default', () => <ExpandyCircleIcon {...defaultProps()} />);
