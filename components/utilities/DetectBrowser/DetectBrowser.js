45ac327918200a125aa21fd092dad0f3c2bada02
JamesIves <iam@jamesiv.es>
2019-04-23 10:14:22 -0400

    Update DetectBrowser.js

--- components/utilities/DetectBrowser/DetectBrowser.js
+++ components/utilities/DetectBrowser/DetectBrowser.js
@@ -45,7 +45,7 @@ export function detectIOS() {
  */
 export function detectMobile() {
   const agent = navigator.userAgent || navigator.vendor || window.opera;
-
+  // Details: https://stackoverflow.com/a/11381730/4933483
   if (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
       agent
@@ -65,7 +65,7 @@ export function detectMobile() {
  */
 export function detectTablet() {
   const agent = navigator.userAgent || navigator.vendor || window.opera;
-
+  // Details: https://stackoverflow.com/a/11381730/4933483
   if (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
       agent
