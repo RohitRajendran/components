11bcb0ca16e0a2d8b5f2757975c6409ca7a540fc
Rohit Rajendran <rohit.rajendran@outlook.com>
2019-05-30 12:08:30 -0400

    Change default story value

--- components/molecules/CardShell/CardShell.story.js
+++ components/molecules/CardShell/CardShell.story.js
@@ -17,7 +17,7 @@ const defaultProps = (isCollapsed = false) => ({
   hasError: boolean('hasError', false),
   hideButton: boolean('hideButton', false),
   isCollapsed: boolean('isCollapsed', isCollapsed),
-  isFetching: boolean('isFetching', true),
+  isFetching: boolean('isFetching', false),
   loading: boolean('loading', false),
   summary: <h1>Collapsed summary content</h1>,
 });
