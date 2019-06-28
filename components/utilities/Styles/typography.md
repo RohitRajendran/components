10211143822f7384aeec026f5519a873af55d6ea
Rohit Rajendran <rohit.rajendran@outlook.com>
2019-06-28 16:55:13 -0400

    Add text style classes (#520)
    * Add text style classes
    
    * Update readme

--- components/utilities/Styles/typography.md
+++ components/utilities/Styles/typography.md
@@ -2,6 +2,14 @@
 
 Here you can find a collection of global text style classes that can be used. This should align with text styles found in [Invision DSM](https://projects.invisionapp.com/dsm/united-income-design-system/united-income-brand/folder/typeStyles/5c10d1eb8f469300123befdf).
 
+## Importing 📦
+
+These classes get imported as a part of the util stylesheet.
+
+```scss
+@import '@unitedincome/components/dist/util.css';
+```
+
 ## Example 🚀
 
 ```javascript
--- components/utilities/Styles/typography.story.js
+++ components/utilities/Styles/typography.story.js
@@ -7,11 +7,28 @@ const stories = storiesOf('Utilities/Styles', module);
 
 const TextStyles = () => (
   <Fragment>
-    <h1>McGonagall Text Styles</h1>
-    <h2 className="uic--mcg-question-text">uic--mcg-question-text</h2>
-    <h2 className="uic--mcg-description-text">uic--mcg-description-text</h2>
-    <h2 className="uic--mcg-subhead-text">uic--mcg-subhead-text</h2>
-    <h2 className="uic--mcg-subhead-2-text">uic--mcg-subhead-2-text</h2>
+    <h2>Base Styles</h2>
+    <div className="uic--header-1-text">uic--header-1-text</div>
+    <div className="uic--header-1-5-text">uic--header-1-5-text</div>
+    <div className="uic--header-2-text">uic--header-2-text</div>
+    <div className="uic--header-3-text">uic--header-3-text</div>
+    <div className="uic--body-text">uic--body-text</div>
+    <div className="uic--stand-alone-cta-text">uic--stand-alone-cta-text</div>
+    <hr />
+    <h2>Library Styles</h2>
+    <div className="uic--library-blog-header-3a">
+      uic--library-blog-header-3a
+    </div>
+    <div className="uic--library-blog-header-3b">
+      uic--library-blog-header-3b
+    </div>
+    <div className="uic--library-pull-quote">uic--library-pull-quote</div>
+    <hr />
+    <h2>McGonagall Text Styles</h2>
+    <div className="uic--mcg-question-text">uic--mcg-question-text</div>
+    <div className="uic--mcg-description-text">uic--mcg-description-text</div>
+    <div className="uic--mcg-subhead-text">uic--mcg-subhead-text</div>
+    <div className="uic--mcg-subhead-2-text">uic--mcg-subhead-2-text</div>
   </Fragment>
 );
 
