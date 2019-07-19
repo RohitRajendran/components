71ebad95f431f412c189d95420c4e3cdd1258cbe
Calvin Mlynarczyk <calvin@unitedincome.com>
2019-07-19 13:42:46 -0400

    Create simple dropdown component (#593)

---
+++ components/atoms/SimpleDropdown/SimpleDropdown.spec.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+import React from 'react';
+import {mount} from 'enzyme';
+import {spy} from 'sinon';
+import SimpleDropdown from './SimpleDropdown';
+
+test('SimpleDropdown - render', (t) => {
+  const onChangeSpy = spy();
+  const component = mount(
+    <SimpleDropdown
+      options={['Yo', 'Red', 'Dawg']}
+      value="Dawg"
+      onChange={onChangeSpy}
+    />
+  );
+
+  // The child component has a weird name for some reason
+  const select = component.find('StateManager');
+
+  t.deepEqual(
+    select.prop('options'),
+    [
+      {label: 'Yo', value: 'Yo'},
+      {label: 'Red', value: 'Red'},
+      {label: 'Dawg', value: 'Dawg'},
+    ],
+    'Pass correct options to Select component'
+  );
+
+  component.instance().onChange({value: 'Yo', label: 'Yo'});
+
+  t.true(onChangeSpy.withArgs('Yo').calledOnce, 'Call onChange with new value');
+
+  t.end();
+});
