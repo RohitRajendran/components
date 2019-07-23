fec9f3084b546b0be5d8fa6990d16d747a0dba67
Rohit Rajendran <rohit.rajendran@outlook.com>
2019-07-23 10:57:26 -0400

    More tooltip tweaks

--- components/atoms/Tooltip/Tooltip.js
+++ components/atoms/Tooltip/Tooltip.js
@@ -42,7 +42,7 @@ const Tooltip = ({
       <Button
         data-tip
         data-for={id}
-        data-event={hover ? 'touchstart mouseover' : 'click '}
+        data-event={hover ? 'touchstart focus mouseover' : 'click'}
         data-event-off={hover ? 'mouseout' : null}
         variant={labelVariant}
         light={lightLabel}
