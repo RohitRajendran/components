0feb5b7d46b281928620bfc3db0ddd835261b7f4
Rohit Rajendran <rohit.rajendran@outlook.com>
2019-08-26 16:55:02 -0400

    [APP-3287] - Strips string of "$" and "," before formatting as currency (#680)
    * Strips string of "$" and "," before formatting as currency
    
    * Rename function

--- components/utilities/FormatUtils/FormatUtils.js
+++ components/utilities/FormatUtils/FormatUtils.js
@@ -1,16 +1,29 @@
 import accounting from 'accounting';
 
+/**
+ * Removes "$" and "," from currency strings
+ * @param {String} currencyString - The number to format
+ * @returns {String} - Returns the formatted number
+ */
+export const cleanCurrency = (currencyString) => {
+  return typeof currencyString === 'string'
+    ? currencyString.replace(/[,$]/g, '')
+    : currencyString;
+};
+
 /** Formats a number into currency.
  * @param {String} numString - The number to format.
  * @returns {String} - Returns the formatted number.
  */
 export const formatCurrency = (numString) => {
+  const value = cleanCurrency(numString);
+
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(Number(numString).toFixed(2));
+  }).format(Number(value).toFixed(2));
 };
 
 /** Formats a number into currency without decimal points.
@@ -18,7 +31,9 @@ export const formatCurrency = (numString) => {
  * @returns {String} - Returns the formatted number.
  */
 export const formatCurrencyNoDecimal = (numString) => {
-  return accounting.formatMoney(Number(numString), {
+  const value = cleanCurrency(numString);
+
+  return accounting.formatMoney(Number(value), {
     symbol: '$',
     precision: 0,
     format: {
