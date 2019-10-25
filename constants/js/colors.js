20e8f8e77e0f86ebc0932b46d99be17eb095a257
James Ives <iam@jamesiv.es>
2019-10-25 16:55:05 -0400

    Exports DSM Colors (#809)

--- constants/js/colors.js
+++ constants/js/colors.js
@@ -4,7 +4,6 @@ export const colors = {
   midnight: '#10004c',
   'middle-purple': '#260070',
   royal: '#4d00ba',
-  'royal-bright': '#6a10e4',
 
   /* Neutrals */
   white: '#ffffff',
@@ -17,10 +16,12 @@ export const colors = {
   pink: '#b30052',
   green: '#008422',
   orange: '#d16b08',
+  yellow: '#dc9f00',
   'disabled-text': '#9fa6bb',
   'super-dark': '#0b0037',
 
   /* Tints & Shades */
+  'green-ondark': '#1ba21f',
   'midnight-90': '#281a5e',
   'midnight-80': '#3f336f',
   'midnight-70': '#584d82',
@@ -30,6 +31,7 @@ export const colors = {
   'midnight-30': '#b7b3c9',
   'midnight-20': '#cfccdb',
   'midnight-10': '#e7e6ed',
+
   'slate-90': '#6b7186',
   'slate-80': '#7c8194',
   'slate-70': '#8c91a1',
@@ -39,4 +41,70 @@ export const colors = {
   'slate-30': '#cdcfd6',
   'slate-20': '#dee0e4',
   'slate-10': '#eeeff1',
+
+  'white-10': 'rgba(255, 255, 255, 0.1)',
+  'white-20': 'rgba(255, 255, 255, 0.2)',
+  'white-30': 'rgba(255, 255, 255, 0.3)',
+  'white-40': 'rgba(255, 255, 255, 0.4)',
+  'white-50': 'rgba(255, 255, 255, 0.5)',
+  'white-60': 'rgba(255, 255, 255, 0.6)',
+  'white-70': 'rgba(255, 255, 255, 0.7)',
+  'white-80': 'rgba(255, 255, 255, 0.8)',
+  'white-90': 'rgba(255, 255, 255, 0.9)',
+
+  /* Graph Shades and Colors */
+  // Solids
+  'graph-robin': '#4db0cf',
+  'graph-turquoise': '#88e6f2',
+  'graph-gray-1': '#a5a8b3',
+  'graph-gray-2': '#dddee1',
+
+  // Gradients
+  'gradient-teal-1': '#e4f9f0',
+  'gradient-teal-2': '#caffe6',
+  'gradient-teal-3': '#acf6f1',
+  'gradient-teal-4': '#7adeea',
+  'gradient-teal-5': '#acf0f6',
+  'gradient-teal-6': '#23b1cd',
+  'gradient-teal-7': '#2cdae8',
+
+  // Pie Tints
+  'gradient-mist-1': '#d6ffed',
+  'gradient-mist-2': '#b0ffda',
+  'gradient-gray-1': '#f7f7f7',
+  'gradient-gray-2': '#e8e8ea',
+  'gradient-gray-3': '#e6e7ec',
+  'gradient-gray-4': '#d1d3d9',
+  'gradient-gray-5': '#b8bcc6',
+
+  'pie-royal-2': '#6524c2',
+  'pie-royal-3': '#8856ce',
+  'pie-royal-4': '#ab87d9',
+  'pie-royal-5': '#ccb9e5',
+  'pie-royal-6': '#e6ddee',
+
+  'pie-d-midnight-2': '#25165a',
+  'pie-d-midnight-3': '#453775',
+  'pie-d-midnight-4': '#695e8f',
+  'pie-d-midnight-5': '#8e85ac',
+  'pie-d-midnight-6': '#b4accc',
+
+  'pie-teal-1': '#23b1cd',
+  'pie-teal-2': '#42bad2',
+  'pie-teal-3': '#6dc9da',
+  'pie-teal-4': '#98d7e1',
+  'pie-teal-5': '#c2e5e9',
+
+  'pie-d-slate-2': '#727992',
+  'pie-d-slate-3': '#8a90a5',
+  'pie-d-slate-4': '#a2a6b5',
+  'pie-d-slate-5': '#c2c5ce',
+
+  'pie-green-1': '#018e54',
+  'pie-green-2': '#259d6c',
+  'pie-green-3': '#56b28c',
+
+  'pie-d-warmgray-1': '#888',
+  'pie-d-warmgray-2': '#a1a1a1',
+  'pie-d-warmgray-3': '#bbb',
 };
