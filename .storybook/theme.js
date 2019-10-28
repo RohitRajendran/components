33453fb225f28cefdedfdff89494c4d665dbd852
James Ives <iam@jamesiv.es>
2019-10-28 16:06:08 -0400

    Theme (#818)

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -5,11 +5,17 @@ import {colors} from '../constants/js/colors';
 // More info: https://storybook.js.org/docs/configurations/theming/
 export default create({
   base: 'light',
-  barSelectedColor: colors.royal,
+  appBg: colors['parchment'],
+  appBorderColor: colors['light-gray'],
+  barBg: colors['white'],
+  barSelectedColor: colors['royal'],
+  colorSecondary: colors['royal'],
+  fontBase: '"Open Sans", sans-serif',
+  textColor: colors['midnight'],
+  inputBg: colors['white'],
+  inputBorder: colors['light-gray'],
+  inputTextColor: colors['midnight'],
   brandTitle: 'United Income',
   brandUrl: 'https://unitedincome.com',
   brandImage: 'https://i.imgur.com/rPP3kdY.png',
-  colorSecondary: colors.midnight,
-  fontBase: '"Open Sans", sans-serif',
-  textColor: colors.midnight,
 });
