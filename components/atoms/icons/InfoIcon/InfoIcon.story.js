1ad3a688d0aa7f68adc4b2c885c828bac88fc121
Calvin Mlynarczyk <calvin@unitedincome.com>
2019-11-26 14:05:41 -0500

    Create info icon (#876)

---
+++ components/atoms/icons/InfoIcon/InfoIcon.story.js
@@ -0,0 +1,22 @@
+import {color, text} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react';
+import React from 'react';
+import InfoIcon from './InfoIcon';
+import InfoIconReadme from './InfoIcon.mdx';
+
+const stories = storiesOf('Atoms/Icons/InfoIcon', module);
+
+stories.addParameters({
+  docs: {
+    page: InfoIconReadme,
+  },
+});
+
+const defaultProps = () => ({
+  circleFill: color('circleFill', '#000'),
+  iconFill: color('iconFill', '#FFF'),
+  height: text('height', '2rem'),
+  width: text('width', '2rem'),
+});
+
+stories.add('default', () => <InfoIcon {...defaultProps()} />);
