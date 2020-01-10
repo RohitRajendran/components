944335da8e156324ae6b85b447da1c57dfe8f264
James Ives <iam@jamesiv.es>
2020-01-10 11:36:47 -0500

    Required Changes (#977)

--- components/utilities/Portal/Portal.story.js
+++ components/utilities/Portal/Portal.story.js
@@ -11,21 +11,25 @@ stories.addParameters({
   },
 });
 
-stories.add('default', () => (
-  <Portal>
-    <div>
-      <h1>Portals children get appended to the document body!</h1>
-    </div>
-    <div>
-      <iframe
-        title="alive"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/Y6ljFaKRTrI"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </div>
-  </Portal>
-));
+stories.add(
+  'default',
+  () => (
+    <Portal>
+      <div>
+        <h1>Portals children get appended to the document body!</h1>
+      </div>
+      <div>
+        <iframe
+          title="alive"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/Y6ljFaKRTrI"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+    </Portal>
+  ),
+  {percy: {skip: true}},
+);
