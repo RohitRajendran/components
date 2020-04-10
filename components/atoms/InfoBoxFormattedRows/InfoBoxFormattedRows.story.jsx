18f4c7e062afe9062e5b3dbbf8c0b61c3657daee
Calvin Mlynarczyk <calvin.mlynarczyk@capitalone.com>
2020-04-10 11:42:51 -0400

    Remove references to InfoBox footer

--- components/atoms/InfoBoxFormattedRows/InfoBoxFormattedRows.story.jsx
+++ components/atoms/InfoBoxFormattedRows/InfoBoxFormattedRows.story.jsx
@@ -34,28 +34,27 @@ stories.add('default', () => (
     title="Giannis - 401k"
     imageURL="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/greek_freak_j2qb.svg"
     content={
-      <InfoBoxFormattedRows
-        {...defaultProps({
-          formName: 'default',
-          rowData: [
-            {
-              label: 'Stocks',
-              value: '95%',
-            },
-            {
-              label: 'Bonds',
-              value: '3%',
-            },
-            {
-              label: 'REITs, Cash, Etc.',
-              value: '2%',
-            },
-          ],
-        })}
-      />
-    }
-    footer={
       <Fragment>
+        <InfoBoxFormattedRows
+          {...defaultProps({
+            formName: 'default',
+            rowData: [
+              {
+                label: 'Stocks',
+                value: '95%',
+              },
+              {
+                label: 'Bonds',
+                value: '3%',
+              },
+              {
+                label: 'REITs, Cash, Etc.',
+                value: '2%',
+              },
+            ],
+          })}
+        />
+        <hr />
         <div className="uic--info-box-formatted-row">
           <div className="uic--row-label">Equity Allocation Override</div>
           <div className="uic--mcg-subhead-text">95%</div>
