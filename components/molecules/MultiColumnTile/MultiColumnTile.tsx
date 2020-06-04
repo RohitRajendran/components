f1d82eccc73194f77039ff125db8ce7218e95d84
James Ives <iam@jamesiv.es>
2020-06-04 10:44:21 -0400

    [APP-5202] Multi Column Tile (#1284)
    * [APP-5202] MultiColumn Tile which represents data in a one and two columns for small and medium to large devices respectively
    
    * addressed PR suggestions and added CloseIcon to exports
    
    Co-authored-by: Jamaal Alleyne <jamaal.alleyne@capitalone.com>

---
+++ components/molecules/MultiColumnTile/MultiColumnTile.tsx
@@ -0,0 +1,121 @@
+/** @module MultiColumnTile */
+import classNames from 'classnames';
+import React, {FC, Fragment, ReactNode} from 'react';
+import './MultiColumnTile.scss';
+import {TileProps} from '../../atoms/Tile/Tile';
+import PaginatedTile from '../PaginatedTile/PaginatedTile';
+
+export type MultiColumnItemsPerPage = {
+  smallDevice: number;
+  mediumDevice: number;
+};
+
+export type MultiColumnProps = {
+  /**Items to be displayed in the tile. */
+  items: React.ReactNode[];
+  /**Items per page for different device sizes*/
+  itemsPerPage?: MultiColumnItemsPerPage;
+} & TileProps;
+
+/**
+ * Gets a single column layout
+ * @param items Array of react nodes
+ * @param obj object consisting of items per page for small devices
+ * @returns List of react nodes
+ */
+const singleColumnLayout = (
+  items: ReactNode[],
+  {smallDevice: itemsPerPage}: MultiColumnItemsPerPage,
+): ReactNode[] => {
+  const pages: React.ReactNode[] = [];
+  for (let page = 0; page < Math.ceil(items.length / itemsPerPage); page++) {
+    pages.push(
+      <div className="uic--multi-column-tile__first-column">
+        {items
+          .slice(page * itemsPerPage, page * itemsPerPage + itemsPerPage)
+          .map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+      </div>,
+    );
+  }
+  return pages;
+};
+
+/**
+ * Gets a multi column layout
+ * @param items Array of react nodes
+ * @param obj object consisting of items per page for medium devices
+ * @returns List of react nodes
+ */
+const multiColumnlayout = (
+  items: ReactNode[],
+  {mediumDevice: itemsPerPage}: MultiColumnItemsPerPage,
+): ReactNode[] => {
+  const pages: React.ReactNode[] = [];
+  for (let page = 0; page < Math.ceil(items.length / itemsPerPage); page++) {
+    const pageItems = items.slice(
+      page * itemsPerPage,
+      page * itemsPerPage + itemsPerPage,
+    );
+
+    const maxItemsPerColumn = Math.ceil(itemsPerPage / 2);
+
+    pages.push(
+      <Fragment>
+        <div className="uic--multi-column-tile__first-column">
+          {pageItems.slice(0, maxItemsPerColumn).map((item, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </div>
+        {pageItems.length > maxItemsPerColumn && (
+          <div className="uic--multi-column-tile__second-column">
+            {pageItems
+              .slice(maxItemsPerColumn, itemsPerPage)
+              .map((item, index) => (
+                <div key={index}>{item}</div>
+              ))}
+          </div>
+        )}
+      </Fragment>,
+    );
+  }
+  return pages;
+};
+
+const MultiColumnTile: FC<MultiColumnProps> = ({
+  items,
+  itemsPerPage = {
+    smallDevice: 5,
+    mediumDevice: 10,
+  },
+  footerContent,
+  className,
+  style,
+}) => {
+  const containerClasses = classNames(
+    {
+      'uic--multi-column-tile': true,
+    },
+    className,
+  );
+
+  return (
+    <div className={containerClasses}>
+      <PaginatedTile
+        pages={singleColumnLayout(items, itemsPerPage)}
+        footerContent={footerContent}
+        className={'uic--d-sm-block uic--d-md-none'}
+        style={style}
+      />
+      <PaginatedTile
+        pages={multiColumnlayout(items, itemsPerPage)}
+        footerContent={footerContent}
+        className={'uic--d-none uic--d-md-block'}
+        style={style}
+      />
+    </div>
+  );
+};
+
+export default MultiColumnTile;
